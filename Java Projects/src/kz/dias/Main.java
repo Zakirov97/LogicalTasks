e6c2651544c1b44f96f4ea78e5e864ebb29240b0
Dias <fire.kiborgik@gmail.com>
2021-08-15 23:26:36 +0600

    second

--- Java Projects/src/kz/dias/Main.java
+++ Java Projects/src/kz/dias/Main.java
@@ -9,16 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
+    
     public static void main(String[] args) {
         
         //ThePowerSum.powerSum(100, 2);
         //ArraysLeftRotation.rotLeft(new ArrayList<>(List.of(1,2,3,4,5)), 7);
         //BillDivision.bonAppetit(new ArrayList<>(List.of(3, 10, 2, 9)),1,12);
-        AppleAndOrange.countApplesAndOranges(7,11,
-                5,15,
-                new ArrayList<>(List.of(-2,2,1)),
-                new ArrayList<>(List.of(5,-6)));
+        //AppleAndOrange.countApplesAndOranges(7, 11, 5, 15, new ArrayList<>(List.of(-2, 2, 1)), new ArrayList<>(List.of(5, -6)));
         
         //System.out.println("sad");
     }
